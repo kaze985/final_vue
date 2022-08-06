95a8562ffb579e1f6552437535d15e6db6659897
7hao <2848279941@qq.com>
2022-08-06 16:11:26 +0800

    修改ws路径

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -57,7 +57,7 @@ export default {
       userInfo: null,
       menuList: [],
       isCollapse: false,
-      wsUrl: 'wss://exchange-prod-7gcbt0vgdd0f6605-1309556468.ap-shanghai.run.wxcloudrun.com', // ws地址
+      wsUrl: 'wss://exchange-prod-7gcbt0vgdd0f6605-1309556468.ap-shanghai.run.wxcloudrun.com/websocket/', // ws地址
       websock: null, // ws实例
       unReadMessage: unReadMessage,
     }
